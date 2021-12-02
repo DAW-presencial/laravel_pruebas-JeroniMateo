32f528048a9d660bcabd8b8766692fa50d2aa75d
Jeroni <jmateo@cifpfbmoll.eu>
2021-12-02 16:40:49 +0100

    cambio sitios

---
+++ formTraslate/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layout');
+
+@section('title','contact')
+
+@endsection
+
+@section('content')
+    <h1>Concact</h1>
+@endsection
---
+++ formTraslate/resources/views/portafolio.blade.php
@@ -0,0 +1,8 @@
+@extends('layout');
+
+@section('title','Portafolio')
+
+
+@section('portafolio')
+    <h1>Portafolio</h1>
+@endsection
