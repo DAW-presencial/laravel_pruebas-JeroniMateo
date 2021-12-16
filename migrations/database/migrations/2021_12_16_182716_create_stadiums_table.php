8833cf80d1d7e81156f526ed4959e68af1bcaa4e
Jeroni <jmateo@cifpfbmoll.eu>
2021-12-16 19:46:22 +0100

    migrations

---
+++ migrations/database/migrations/2021_12_16_182716_create_stadiums_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStadiumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stadiums', function (Blueprint $table) {
+            $table->increments('stadium_id');
+            $table->string('stadium_name');
+            $table->integer('capacity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stadiums');
+    }
+}
