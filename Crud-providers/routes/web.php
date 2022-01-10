aaf4f692c9fdf4b30f0fd7ba232c79d5e79d907d
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-10 20:52:42 +0100

    Controller Contactos Controller

--- Crud-providers/routes/web.php
+++ Crud-providers/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__. 'auth.php';
+
+Route::resource('cuentas', ContactosController::class);
+    
+
