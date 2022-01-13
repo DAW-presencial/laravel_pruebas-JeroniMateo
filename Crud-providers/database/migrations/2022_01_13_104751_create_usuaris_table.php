a0267da07894219e420206b0660ce285bd9e7064
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-13 11:55:13 +0100

    migrations & seeders creation
    "Usuaris & Contactos"

--- Crud-providers/database/migrations/2022_01_13_104751_create_usuaris_table.php
+++ Crud-providers/database/migrations/2022_01_13_104751_create_usuaris_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsuarisTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('usuaris', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('apellidos');
+            $table->string('email');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuaris');
     }
 }
