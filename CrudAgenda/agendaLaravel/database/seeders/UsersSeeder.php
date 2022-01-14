501d8beaeba4c17adec8b992f690681aecef2518
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-14 16:39:37 +0100

    Seeder User creation

--- CrudAgenda/agendaLaravel/database/seeders/UsersSeeder.php
+++ CrudAgenda/agendaLaravel/database/seeders/UsersSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -15,7 +17,11 @@ class UsersSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            
+            'name'=>'Jeroni',
+            'email'=>'jmateo@cifpfbmoll.eu',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secretos'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
