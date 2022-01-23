4bbb4c50eaddc93aa92b3f21d347184ee7b86404
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-23 09:30:59 +0100

    lang ca es

--- CrudAgenda/agendaLaravel/database/seeders/ContactosSeeder.php
+++ CrudAgenda/agendaLaravel/database/seeders/ContactosSeeder.php
@@ -15,14 +15,19 @@ class ContactosSeeder extends Seeder
     public function run()
     {
         DB::table('contactos')->insert([
-            'nombre'=>'Jeroni',
-            'telefono'=>'123456789',
-            'tipo'=>'Movil'
+            'nombre' => 'Jeroni',
+            'telefono' => '123456789',
+            'tipo' => 'Movil'
         ]);
         DB::table('contactos')->insert([
-            'nombre'=>'Rafael',
-            'telefono'=>'987654321',
-            'tipo'=>'Fijo'
+            'nombre' => 'Rafael',
+            'telefono' => '987654321',
+            'tipo' => 'Fijo'
+        ]);
+        DB::table('contactos')->insert([
+            'nombre' => 'Toni',
+            'telefono' => '342340123',
+            'tipo' => 'Movil'
         ]);
     }
 }
--- CrudAgenda/agendaLaravel/resources/lang/ca/passwords.php
+++ CrudAgenda/agendaLaravel/resources/lang/ca/passwords.php
@@ -12,9 +12,9 @@ return [
     |
     */
 
-    'password' => 'Las contraseñas deben coincidir y contener al menos 6 caracteres',
-    'reset'    => '¡Tu contraseña ha sido restablecida!',
-    'sent'     => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
-    'token'    => 'El token de recuperación de contraseña es inválido.',
-    'user'     => 'No podemos encontrar ningún usuario con ese correo electrónico.',
+    'password' => 'Les contrasenyes han de contenir almenys 6 caràcters i coincidir.',
+    'reset'    => 'La contrasenya s\'ha restablert!',
+    'sent'     => 'T\'hem enviat per e-mail un enllaç per a reiniciar la teva contrasenya!',
+    'token'    => 'Aquest token de recuperació de contrasenya és invàlid.',
+    'user'     => 'No existeix cap usuari amb aquest correu.',
 ];
