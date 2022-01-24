fb5626cf41490065d83925347aaab10f8dc5bb77
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-24 19:44:00 +0100

    reference between contacts and phonebook

---
+++ CrudAgenda/agendaLaravel/database/migrations/2022_01_24_182210_create_phonebooks_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonebooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phonebooks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        
+        Schema::table('contactos', function (Blueprint $table) {
+            $table->unsignedBigInteger('phonebook_id')->nullable()->after('id');
+
+            $table->foreign('phonebook_id')->references('id')->on('phonebooks')
+                ->onDelete('cascade')
+                ->onUpdate('set null');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contactos', function (Blueprint $table) {
+            $table->dropForeign('phonebook_category_id_foreign');
+           $table->dropColumn('phonebook_id');
+        });
+
+
+        Schema::dropIfExists('phonebooks');
+    }
+}
